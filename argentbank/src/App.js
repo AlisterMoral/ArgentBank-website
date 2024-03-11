5eff1a7540a0d5204c7716fa3957282f7a4d255d
Alister <Alister.moral@gmail.com>
2024-03-11 13:04:45 +0100

    installation application et routages

---
+++ argentbank/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import User from './pages/User.JS';
+import SignIn from './pages/SignIn';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
